6e27b721aedf0eb95e90222896b24c44e59e3fe9
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-23 19:20:49 +0530

    Updates

---
+++ precache-manifest.03f77c407c69c03567b7cf5918f980c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dafdd08cf90f33bc5c51d58cfa7445a",
+    "url": "/tool22/index.html"
+  },
+  {
+    "revision": "fde9e587a659e8b9ffe2",
+    "url": "/tool22/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b6ed73116a1f750559ee",
+    "url": "/tool22/static/css/main.0963664c.chunk.css"
+  },
+  {
+    "revision": "fde9e587a659e8b9ffe2",
+    "url": "/tool22/static/js/2.efc6c4a3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tool22/static/js/2.efc6c4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ed73116a1f750559ee",
+    "url": "/tool22/static/js/main.a2c950b2.chunk.js"
+  },
+  {
+    "revision": "4e1d57e584ad0102f404",
+    "url": "/tool22/static/js/runtime-main.5fe89d68.js"
+  }
+]);
